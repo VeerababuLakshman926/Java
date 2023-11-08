cc2c9daf4f7c7c4165692b51e8f499ac0949f46c
VeerababuLakshman926 <mattaparthilakshmanaveerababu@gmail.com>
2023-11-08 15:25:11 +0530

    v1

---
+++ 2nov/2.Firstapp.java
@@ -0,0 +1,11 @@
+class Firstapp{
+    public static void main(String[] args){
+        //System.out.println("hello");
+        //System.out.println(args[0]+ args[1]);
+        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+    }
+
+    
+}
+
+//Class name is not case sensitive but we follow the convention that it always starts with uppercase.
